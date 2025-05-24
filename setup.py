fcd21e5da33ead828b000ded2e654b2b6df9da90
j0c0p72 <jae.choi@samsclub.com>
2025-05-23 22:47:40 -0500

    updated the requirements and setup to unify the required packages

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
+def read_requirements(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
 setup(
     name="demo-csv-linter",
     version="0.0.1",
     description="demo python CLI tool to lint csv files",
     packages=find_packages(),
-    install_requires=[
-        "click==7.1.2",
-        "numpy==1.24.3",
-        "pandas==1.5.3"
-    ],
+    install_requires=read_requirements('requirements.txt'),
     entry_points={
         "console_scripts": [
             "csv-linter=csv_linter.main:main"
